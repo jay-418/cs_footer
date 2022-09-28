2fa92a533fb9c33913830cba0641f2e50b038420
Jay <110402935+jay-418@users.noreply.github.com>
2022-09-28 18:38:55 -0500

    added nestled divs to work around jitbit class sanitation

--- minify.py
+++ minify.py
@@ -1,11 +1,12 @@
-#input file
+# Original file
 fin = open("footer.html", "rt")
-#output file to write the result to
+
+# Modified file
 fout = open("footer-min.html", "wt")
-#for each line in the input file
+
 for line in fin:
-	#read replace the string and write to output file
+	# Remove each line break
 	fout.write(line.replace('\n', ''))
-#close input and output files
+
 fin.close()
 fout.close()
